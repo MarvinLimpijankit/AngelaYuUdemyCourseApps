56552bf421ceeceaee45fb936eaa0ccf1bb0d4c9
MarvinLimpijankit <75536446+MarvinLimpijankit@users.noreply.github.com>
2022-01-11 15:39:54 +0700

    Add files via upload

---
+++ BMI-Calculator-iOS13/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Marvin Limpijankit on 7/22/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
+
